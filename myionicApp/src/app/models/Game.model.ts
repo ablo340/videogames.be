156180c4c08859a555f1070a392201bc8a3f6f27
Kolawole Abdoulaye <ablo110@live.fr>
2020-03-16 23:25:20 +0100

    delete some files for reoganisation

--- myionicApp/src/app/models/Game.model.ts
+++
@@ -1,9 +0,0 @@
-export class Game {
-    nom: String
-    constructor(nom: String,    
-        genre: String,
-        commentaire: String,
-        image: string,
-        date_de_sortie: String,
-        console: any ){}
-}
